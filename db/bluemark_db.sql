38fb1a4ce2355c1d9a8cf3b02f83b315828f0f1c
zainnayani <43630494+zainnayani@users.noreply.github.com>
2019-10-20 20:52:28 +0500

    Database

--- db/bluemark_db.sql
+++ db/bluemark_db.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2019 at 11:35 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.10
+-- Generation Time: Oct 20, 2019 at 05:50 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -29,39 +27,86 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `admin_id` int(11) NOT NULL,
+  `ad_id` int(11) NOT NULL,
+  `f_name` varchar(255) NOT NULL,
+  `l_name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `p.num` varchar(15) NOT NULL,
+  `identity` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `shop_id` int(11) NOT NULL,
-  `firstname` varchar(250) NOT NULL,
-  `lastname` varchar(250) NOT NULL,
-  `phone_num` int(50) NOT NULL,
-  `password` varchar(250) NOT NULL
+  `uni_id` int(11) NOT NULL,
+  `airport_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `master_admin`
+-- Table structure for table `airport`
 --
 
-CREATE TABLE `master_admin` (
-  `m_admin_id` int(11) NOT NULL,
-  `admin_id` int(11) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL
+CREATE TABLE `airport` (
+  `airport_id` int(11) NOT NULL,
+  `airport_name` varchar(255) NOT NULL,
+  `hardware_id` int(11) NOT NULL,
+  `airway_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `notification`
+-- Table structure for table `airway`
 --
 
-CREATE TABLE `notification` (
-  `no_id` int(11) NOT NULL,
-  `title` varchar(250) NOT NULL,
-  `description` varchar(250) NOT NULL,
-  `image` varchar(250) NOT NULL,
-  `shop_id` int(11) NOT NULL
+CREATE TABLE `airway` (
+  `airway_id` int(11) NOT NULL,
+  `airway_name` varchar(255) NOT NULL,
+  `imformation_title` varchar(120) NOT NULL,
+  `information_des` varchar(500) NOT NULL,
+  `flight_name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hardware`
+--
+
+CREATE TABLE `hardware` (
+  `hardware_id` int(11) NOT NULL,
+  `hardware_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) DEFAULT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `offer_title` varchar(120) NOT NULL,
+  `offer_des` varchar(500) NOT NULL,
+  `brand_name` varchar(50) NOT NULL,
+  `image` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `program`
+--
+
+CREATE TABLE `program` (
+  `program_id` int(11) NOT NULL,
+  `program_name` varchar(255) NOT NULL,
+  `course_name` varchar(255) NOT NULL,
+  `update_title` varchar(120) NOT NULL,
+  `update_des` varchar(500) NOT NULL,
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -72,9 +117,22 @@ CREATE TABLE `notification` (
 
 CREATE TABLE `shop` (
   `shop_id` int(11) NOT NULL,
-  `shop_name` varchar(250) NOT NULL,
-  `shop_no` varchar(150) NOT NULL,
-  `owner_name` varchar(250) NOT NULL
+  `shop_name` varchar(255) NOT NULL,
+  `hardware_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uni`
+--
+
+CREATE TABLE `uni` (
+  `uni_id` int(11) DEFAULT NULL,
+  `uni_name` varchar(255) NOT NULL,
+  `hardware_id` int(11) NOT NULL,
+  `program_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -84,12 +142,12 @@ CREATE TABLE `shop` (
 --
 
 CREATE TABLE `user` (
-  `u_id` int(11) NOT NULL,
-  `firstname` varchar(250) NOT NULL,
-  `lastname` varchar(250) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL,
-  `confirm_password` varchar(250) NOT NULL
+  `user_id` int(11) DEFAULT NULL,
+  `f_name` varchar(255) NOT NULL,
+  `l_name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -100,34 +158,51 @@ CREATE TABLE `user` (
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`admin_id`),
-  ADD UNIQUE KEY `shop_id` (`shop_id`);
+  ADD PRIMARY KEY (`ad_id`),
+  ADD UNIQUE KEY `shop_id` (`shop_id`),
+  ADD UNIQUE KEY `uni_id` (`uni_id`),
+  ADD UNIQUE KEY `airport_id` (`airport_id`);
 
 --
--- Indexes for table `master_admin`
+-- Indexes for table `airport`
 --
-ALTER TABLE `master_admin`
-  ADD PRIMARY KEY (`m_admin_id`),
-  ADD UNIQUE KEY `admin_id` (`admin_id`);
+ALTER TABLE `airport`
+  ADD PRIMARY KEY (`airport_id`),
+  ADD UNIQUE KEY `hardware_id` (`hardware_id`),
+  ADD UNIQUE KEY `campany_id` (`airway_id`);
 
 --
--- Indexes for table `notification`
+-- Indexes for table `airway`
 --
-ALTER TABLE `notification`
-  ADD PRIMARY KEY (`no_id`),
-  ADD UNIQUE KEY `shop_id` (`shop_id`);
+ALTER TABLE `airway`
+  ADD PRIMARY KEY (`airway_id`);
+
+--
+-- Indexes for table `hardware`
+--
+ALTER TABLE `hardware`
+  ADD PRIMARY KEY (`hardware_id`);
+
+--
+-- Indexes for table `program`
+--
+ALTER TABLE `program`
+  ADD PRIMARY KEY (`program_id`);
 
 --
 -- Indexes for table `shop`
 --
 ALTER TABLE `shop`
-  ADD PRIMARY KEY (`shop_id`);
+  ADD PRIMARY KEY (`shop_id`),
+  ADD UNIQUE KEY `hardware_id` (`hardware_id`),
+  ADD UNIQUE KEY `product_id` (`product_id`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `uni`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`u_id`);
+ALTER TABLE `uni`
+  ADD UNIQUE KEY `hardware_id` (`hardware_id`),
+  ADD UNIQUE KEY `program_id` (`program_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -137,33 +212,32 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `ad_id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `master_admin`
+-- AUTO_INCREMENT for table `airport`
 --
-ALTER TABLE `master_admin`
-  MODIFY `m_admin_id` int(11) NOT NULL AUTO_INCREMENT;
-
+ALTER TABLE `airport`
+  MODIFY `airport_id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `notification`
+-- AUTO_INCREMENT for table `airway`
 --
-ALTER TABLE `notification`
-  MODIFY `no_id` int(11) NOT NULL AUTO_INCREMENT;
-
+ALTER TABLE `airway`
+  MODIFY `airway_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `hardware`
+--
+ALTER TABLE `hardware`
+  MODIFY `hardware_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `program`
+--
+ALTER TABLE `program`
+  MODIFY `program_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `shop`
 --
 ALTER TABLE `shop`
   MODIFY `shop_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
